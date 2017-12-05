95efe54770e0ed9d23763569de9ef24f462ce369
Capt.Ace <capt4ce@localhost.localdomain>
2017-12-05 21:38:26 +0530

    hadling google api request on progress

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from DExyS import main as dexys
 from flask import jsonify
 from flask import Flask, redirect,render_template, url_for, request 
+import requests
 from flask_cors import CORS
 
 
@@ -32,9 +33,29 @@ def search():
                 return jsonify(req)
          
         else:
+            # query=request.args.get('query')
+            # keywords,suggested_keywords, result=dexys.querySearch(query)
+            # res = {'success':True,'data':result,'search_keywords':keywords,'suggested_keywords':suggested_keywords}
+            # return jsonify(res)
             query=request.args.get('query')
-            keywords,suggested_keywords, result=dexys.querySearch(query)
-            res = {'success':True,'data':result,'search_keywords':keywords,'suggested_keywords':suggested_keywords}
+            # keywords,suggested_keywords, #result=dexys.querySearch(query)
+            data=[]
+            ky=[]
+            url='https://www.googleapis.com/customsearch/v1?q='+query+'&key=AIzaSyASWpsbC2yHZZA7IVQWuHGTLbFr6I7y6XI&cx=001234642073522909578:dw7qnkw1euk'
+            print(url)
+            s1=requests.get(url)
+            print(s1)
+            # pr=s1['response']['items']
+            # for d in pr:
+            #     data.append({name:pr.link,description:pr.snippet})
+
+            # s2=requests.get('http://api.bing.com/osjson.aspx?query='+query)
+            # pr=s2[1]
+            # for idx in range(len(pr)):
+            #     if idx!=0:
+            #         ky.append({name:pr[idx]})
+
+            res = {'success':True,'data':data,'suggested_keywords':ky}
             return jsonify(res)
 
 if __name__ == '__main__':
