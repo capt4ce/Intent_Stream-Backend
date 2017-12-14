07cffa8cd8a28b115d0f2c7afdbed639630626ba
Capt.Ace <capt4ce@localhost.localdomain>
2017-12-14 06:36:21 +0530

    fixed post tags: with help of NLP tokenization, refine keywords to include words from query as well

--- setup.py
+++ setup.py
@@ -3,7 +3,41 @@ from flask import jsonify
 from flask import Flask, redirect,render_template, url_for, request 
 import requests
 import json
+import ast
 from flask_cors import CORS
+import nltk
+
+# def google_search_dummy():
+#     a ="""
+#             {
+#         "items": [
+#             {
+#             "kind": "customsearch#result",
+#             "title": "Computer - Wikipedia",
+#             "displayLink": "en.wikipedia.org",
+#             "snippet": "A computer is a device that can be instructed to carry out arbitrary sequences of \narithmetic or logical operations automatically. The ability of computers to follow \ngeneralized sets of operations, called programs, enables them to perform an \nextremely wide range of tasks. Such computers are used as control systems for a \nvery ...",
+#             "cacheId": "fCBvEeFb9p8J",
+#             "pagemap": {
+#                 "cse_thumbnail": [
+#                 {
+#                 "width": "119",
+#                 "height": "79",
+#                 }
+#                 ],
+#                 "metatags": [
+#                 {
+#                 "referrer": "origin-when-cross-origin"
+#                 }
+#                 ],
+#                 "cse_image": [
+
+#                 ]
+#             }
+            
+#             ]
+#             }
+#     """
+#     return json.loads(a)
 
 
 app = Flask(__name__)
@@ -41,7 +75,7 @@ def search():
 
 
             query=request.args.get('query') #   .lower()
-            qSplitted = query.lower().split()
+            qSplitted = query.split()
             # keywords,suggested_keywords, #result=dexys.querySearch(query)
             data=[]
             ky=[]
@@ -49,10 +83,13 @@ def search():
             print(url)
             s1=requests.get(url)
             s2 = json.loads(s1.text)
+            # s2=google_search_dummy
             pr=s2["items"]
             # print(pr)
             for d in pr:
-                data.append({"name":d["title"],"link":d["link"],"description":d["snippet"], "tags":["aaa","bbb","ccc"]})
+                tagsTokens=nltk.word_tokenize(d["title"])
+                filteredTags = [w for w in tagsTokens if len(w)>3]
+                data.append({"name":d["title"],"link":d["link"],"description":d["snippet"], "tags":list(set(filteredTags))})
 
             s1=requests.get('http://api.bing.com/osjson.aspx?query='+query)
             s2=json.loads(s1.text)
@@ -61,13 +98,20 @@ def search():
                 if idx!=0:
                     word = pr[idx]
                     for q in qSplitted:
+                        q=q.lower()
                         word = word.replace(q,'')
                     ky.append({"title":word.strip()})
 
-            res = {'success':True,'data':data,'suggested_keywords':ky, 'query':query}
+            print(str(qSplitted))
+            print(str(ky))
+            formatedQSplit = []
+            for q in qSplitted:
+                formatedQSplit.append({"title":q})
+            keywords = formatedQSplit+ky
+            print(str(keywords))
+
+            res = {'success':True,'data':data,'suggested_keywords':keywords, 'query':query}
             return jsonify(res)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
